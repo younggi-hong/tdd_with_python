b0e6f7db98afd5c592bb9ae0d972857f92304e93
yghong <todqka83@gmail.com>
2015-02-16 00:54:38 +0900

    three strikes and refactor

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 	def test_root_url_resolves_to_home_page_view(self):
@@ -13,4 +14,14 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(), expected_html)
+	
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+		response = home_page(request)	
+		self.assertIn('A new list item', response.content.decode())
+		expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
+		self.assertEqual(response.content.decode(), expected_html)
+
 
--- lists/views.py
+++ lists/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
+#	if request.method == 'POST':
+#		return HttpResponse(request.POST['item_text'])
 #	return HttpResponse('<html><title>To-Do lists</title></html>')
-	return render(request, 'home.html')
+	return render(request, 'home.html', {'new_item_text': request.POST.get('item_text',''),})
 
