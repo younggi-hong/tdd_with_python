8379727926efc0a8484a561b35b6dbac0d3cee66
yghong <todqka83@gmail.com>
2015-02-18 16:15:58 +0900

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
@@ -15,7 +15,7 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
@@ -32,26 +32,7 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
-	
-		#import time
-		#time.sleep(10)
-
-		#table = self.browser.find_element_by_id('id_list_table')
-		#rows = table.find_elements_by_tag_name('tr')
-		
-		#self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-		#"New to-do item did not apear in table"
-		#)
-
-		#self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		#self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
-
-
 		self.fail('Finish the test!')
-
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
 
 
 
