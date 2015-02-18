a90b89abe5f4c2f982a01060ce33df1f15bb469a
yghong <todqka83@gmail.com>
2015-02-18 15:58:15 +0900

    redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -21,9 +21,9 @@ class HomePageTest(TestCase):
 		request.method = 'POST'
 		request.POST['item_text'] = 'A new list item'
 		response = home_page(request)	
-		self.assertIn('A new list item', response.content.decode())
-		expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
-		self.assertEqual(response.content.decode(), expected_html)
+		#self.assertIn('A new list item', response.content.decode())
+		#expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
+		#self.assertEqual(response.content.decode(), expected_html)
 
 class ItemModelTest(TestCase):
 	def test_saving_retrieving_items(self):
@@ -43,5 +43,36 @@ class ItemModelTest(TestCase):
 		self.assertEqual(first_saved_item.text, 'The first list item')
 		self.assertEqual(second_saved_item.text, 'Item the second')
 
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+		response = home_page(request)
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+	def test_home_page_redirects_after_POST(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+		response = home_page(request)
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+
+	def test_home_page_only_saves_items_when_necessary(self):
+		request = HttpRequest()
+		home_page(request)
+		self.assertEqual(Item.objects.count(), 0)
+	
+	def test_home_page_displays_all_list_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		request = HttpRequest()
+		response = home_page(request)
+
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2', response.content.decode())
 
 
