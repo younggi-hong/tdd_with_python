7e0286b2419a766a439490e88c4a0582a69a4afb
yghong <todqka83@gmail.com>
2015-05-02 18:29:48 +0900

    use form in all views, back to working state

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -12,11 +12,11 @@ class LayoutAndStylingTest(FunctionalTest):
 		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024,768)
 
-		inputbox = self.browser.get_item_input_box()
+		inputbox = self.get_item_input_box()
 		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
 
 		inputbox.send_keys('testing\n')
-		inputbox = self.browser.get_item_input_box()
+		inputbox = self.get_item_input_box()
 		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
 
 	
