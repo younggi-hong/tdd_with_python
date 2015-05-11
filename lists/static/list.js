11063e09cf05984a8b1549ea093a27af85dc948c
yghong <todqka83@gmail.com>
2015-05-11 23:17:22 +0900

    Deploying Our New Code

---
+++ lists/static/list.js
@@ -0,0 +1,11 @@
+$('input').on('keypress', function () {
+	$('.has-error').hide();
+});
+
+jQuery(document).ready(function $() {
+	$('input').on('keypress', function() {
+		$('.has-error').hide();
+	});
+}); 
+
+
